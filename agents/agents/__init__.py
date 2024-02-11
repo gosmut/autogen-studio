fa19b9dec1a19e17ec61c8ba5eb2688983e799d4
gosmut <151729406+gosmut@users.noreply.github.com>
2024-02-11 10:13:58 +0100

    Delete agents/agents/__init__.py

--- agents/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_agent import BaseAgent
-from .functionner import Functioneer
-from .product_owner import ProductOwner
-from .software_engineer import SoftwareEngineer
-from .user_experience_designer import UserExperienceDesigner
